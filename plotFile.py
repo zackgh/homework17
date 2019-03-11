84300bffc175fce07f88dd0595d0cda21e3e0a5f
Zack Ghalayini <zackghalayini@Zacks-MacBook-Pro.local>
2019-03-11 16:01:32 -0600

    Added some comments

--- plotFile.py
+++ plotFile.py
@@ -1,5 +1,20 @@
+
 import matplotlib.pyplot as plt 
 xValue = [1,2,3,4]
 yValue = [5,6,7,8]
 plt.plot(xValue,yValue)
 plt.show()
+
+"""
+Git steps 
+
+1. Make changes and save to file 
+2. git add filename to to add to the "to be submitted"
+3. git commit -m "comments" 
+4. then git push last 
+
+to run python in vim 
+
+:!python % 
+% is this file 
+"""
